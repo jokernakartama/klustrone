87900ee6ad568ae0a6232a237e3e3929fb92d6df
zzap <eat.yummies@gmail.com>
2018-02-18 09:21:53 +0300

    Added index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { start as startSession } from '~/utils/session'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './store'
+
+// exclude some sessionStorage keys from crosstabs session
+const excludedSessionKeys = [
+  'sort'
+]
+
+startSession(excludedSessionKeys).then(
+  function () {
+    ReactDOM.render(
+      <Provider store={ store }>
+        <App />
+      </Provider>,
+      document.getElementById('app')
+    )
+  }
+)
