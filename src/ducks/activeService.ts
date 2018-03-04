b920dc4d040b94233dc48dc4cf6ec0802422ee72
zzap <eat.yummies@gmail.com>
2018-03-04 14:39:13 +0300

    Added activeService.ts reducer

---
+++ src/ducks/activeService.ts
@@ -0,0 +1,40 @@
+type actionPayload = string|null
+
+interface IAction {
+    type?: string
+    payload?: actionPayload
+}
+
+const initialState = null
+
+export const SERVICE_SELECT = 'activeservice::select'
+export const SERVICE_CLEAR = 'activeservice::clear'
+
+export function selectService (service: actionPayload): IAction {
+  return {
+    type: SERVICE_SELECT,
+    payload: service
+  }
+}
+
+export function clearServiceSelection (): IAction {
+  return {
+    type: SERVICE_CLEAR
+  }
+}
+
+// REDUCER
+
+const actionsMap = {
+  [SERVICE_SELECT ]: (state, action) => {
+    return action.payload
+  },
+  [SERVICE_CLEAR]: () => {
+    return null
+  }
+}
+
+export default function reducer (state: actionPayload = initialState, action: IAction = {}): actionPayload {
+  const fn = actionsMap[action.type]
+  return fn ? fn(state, action) : state
+}
