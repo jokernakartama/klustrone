d38e0ffb154005f73c3692448cc5708d665eb1a4
zzap <eat.yummies@gmail.com>
2018-05-02 18:14:54 +0300

    Added tests for resourceIsTrash.ts

---
+++ src/ducks/resourceIsTrash/__tests__/resourceIsTrash.spec.js
@@ -0,0 +1,28 @@
+import thunk from 'redux-thunk'
+import * as dux from '../resourceIsTrash'
+import reducer from '../resourceIsTrash'
+
+describe('ducks/resourceIsTrash.ts', function() {
+  describe('updateDir', function() {
+    it('should create an action to update trash flag', function () {
+      const isTrash = true
+      const expected = {
+        type: dux.RESOURCES_TRASH_FLAG_UPDATE,
+        payload: isTrash
+      }
+      const action = dux.updateTrashFlag(isTrash)
+      expect(action).to.deep.equal(expected)
+    })
+  })
+  describe('resourceIsTrash reducer', function() {
+    it('should handle RESOURCES_TRASH_FLAG_UPDATE', function () {
+      const isTrash = false
+      const expected = {
+        type: dux.RESOURCES_TRASH_FLAG_UPDATE,
+        payload: isTrash
+      }
+      const action = dux.updateTrashFlag(isTrash)
+      expect(reducer(true, action)).to.equal(isTrash)
+    })
+  })
+})
