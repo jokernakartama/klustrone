d59d40153a2215c4b1becb8753c7711a6e817b3a
zzap <eat.yummies@gmail.com>
2018-05-12 22:38:23 +0300

    Refactored ResourceListView component

--- src/components/ResourceListView/ResourceListView.tsx
+++ src/components/ResourceListView/ResourceListView.tsx
@@ -1,6 +1,7 @@
 import './styles/resourceListView.styl'
 import React from 'react'
 import ScrollArea from '~/components/ScrollArea'
+import bem from '~/utils/bemName'
 
 interface IResourceListViewComponentProps {
   children?: React.ReactNode
@@ -8,8 +9,9 @@ interface IResourceListViewComponentProps {
 
 const ResourceListView: React.SFC<IResourceListViewComponentProps> = (props) => {
   const blockName = 'resource-list'
+  const wrapperClassName = bem({ block: blockName + '-wrapper' })
   return (
-    <div className={ blockName + '-wrapper' }>
+    <div className={ wrapperClassName }>
       <ScrollArea className={ blockName }>
         { props.children }
       </ScrollArea>
