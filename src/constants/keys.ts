53fef88da0708a166b03fc20164dcfb93a659ef0
zzap <eat.yummies@gmail.com>
2018-05-27 16:41:10 +0300

    Added key prefix for buffer

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -1,7 +1,8 @@
 enum keys {
   VIEW_SETTINGS = 'view',
   SORT_SETTINGS = 'sort',
-  TOKEN_BAG_PREFIX = 'token_'
+  TOKEN_BAG_PREFIX = 'token_',
+  BUFFER_PREFIX = 'buffer_'
 }
 
 export default keys
