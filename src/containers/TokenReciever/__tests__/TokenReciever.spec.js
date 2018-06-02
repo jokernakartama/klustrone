5f9bbf8d7bb66e9d5daadd2a874ed78ae7d63c7f
zzap <eat.yummies@gmail.com>
2018-06-02 15:01:16 +0300

    Added tests for TokenReciever container

---
+++ src/containers/TokenReciever/__tests__/TokenReciever.spec.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import TokenReciever from '../TokenReciever'
+
+describe('Component <TokenReciever />', () => {
+  it('should render presentational component as an endpoint', () => {
+    const wrapper = enzyme.shallow(<TokenReciever />)
+    expect(wrapper.name()).to.equal('TokenRecieverView')
+  })
+})
