422e9d28ea6643b5d082ef31809d7658120d1f67
zzap <eat.yummies@gmail.com>
2018-06-02 18:09:03 +0300

    Removed karma-mocha-reporter

--- build/config/karma.config.js
+++ build/config/karma.config.js
@@ -4,33 +4,25 @@ module.exports = function(config) {
   config.set({
     files: [
       // use a polyfill for function.prototype.bind which is missing from PhantomJS
-      'node_modules/phantomjs-polyfill/bind-polyfill.js',
-      'node_modules/babel-polyfill/browser.js',
-      'build/config/enzyme.init.js',
+      { pattern: 'node_modules/phantomjs-polyfill/bind-polyfill.js', watched: false},
+      { pattern: 'node_modules/babel-polyfill/browser.js', watched: false},
+      { pattern: 'build/config/enzyme.init.js', watched: false},
       // path pattern for specifications
-      'src/**/*.spec.js'
+      'src/**/*.spec.js',
     ],
     // frameworks to use in test files
     frameworks: ['mocha', 'chai-sinon'],
     basePath: '../../',
     preprocessors: {
-      // application files
       'build/config/enzyme.init.js': ['webpack'],
+      // application files     
       'src/**/*.js': ['webpack'],
       'src/**/*.tsx': ['webpack'],
       'src/**/*.ts': ['webpack'],
     },
     logLevel : config.LOG_DISABLE,
-    reporters: ['mocha'],
-    mochaReporter: {
-      maxLogLines: -1,
-      showDiff: true
-    },
+    reporters: ['spec'],
     webpack: webpackConfig,
-    webpackMiddleware: {
-      logLevel: 'silent',
-      stats: 'errors-only'
-    },
     browsers: ['PhantomJS']
   })
 }
