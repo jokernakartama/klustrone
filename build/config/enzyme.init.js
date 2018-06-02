02d03e8b5465b440b7d1f6719a210524238d0794
zzap <eat.yummies@gmail.com>
2018-06-02 17:35:36 +0300

    Changed enzyme api namespace for tests

--- build/config/enzyme.init.js
+++ build/config/enzyme.init.js
@@ -2,6 +2,8 @@ var Enzyme = require('../../node_modules/enzyme')
 var Adapter = require('../../node_modules/enzyme-adapter-react-16')
 Enzyme.configure({adapter: new Adapter()})
 
-window.shallow = Enzyme.shallow
-window.mount = Enzyme.mount
-window.render = Enzyme.render
+window.enzyme = {
+  shallow: Enzyme.shallow,
+  mount: Enzyme.mount,
+  render: Enzyme.render
+}
