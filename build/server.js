7586182d42839dae385d04a6a319b099eae21493
zzap <eat.yummies@gmail.com>
2018-06-19 21:05:15 +0300

    Modified dev server

--- build/server.js
+++ build/server.js
@@ -31,7 +31,7 @@ const hotMiddleware = require('webpack-hot-middleware')(compiler, {
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({ action: 'reload' })
-    cb()
+    if (cb) cb()
   })
 })
 
