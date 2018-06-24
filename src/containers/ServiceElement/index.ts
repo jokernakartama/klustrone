4e2d58efe3515f40c02a9a262439e2e145936fb3
zzap <eat.yummies@gmail.com>
2018-06-24 18:23:24 +0300

    Removed containers: ServiceList, ServiceElement

--- src/containers/ServiceElement/index.ts
+++
@@ -1,3 +0,0 @@
-import { default as _ServiceElement } from './ServiceElement'
-const ServiceElement = _ServiceElement
-export default ServiceElement
--- src/containers/ServiceList/index.ts
+++
@@ -1,3 +0,0 @@
-import { default as _ServiceList } from './ServiceList'
-const ServiceList = _ServiceList
-export default ServiceList
