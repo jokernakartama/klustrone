891f33c78468f5b4574d79796e5b3b2bef9ebefe
zzap <eat.yummies@gmail.com>
2018-06-25 14:26:33 +0300

    Modified UIScrollAreaView component tests

--- src/components/UI/ScrollArea/View/__tests__/UIScrollAreaView.spec.js
+++ src/components/UI/ScrollArea/View/__tests__/UIScrollAreaView.spec.js
@@ -2,17 +2,17 @@ import React from 'react'
 import UIScrollAreaView from '../UIScrollAreaView'
 
 const ChildComponent = (props) => {
-  return <div className='child-component'>Any content</div>
+  return <div className='child-component' style={ { height: '300px' } }>Any content</div>
 }
 
-describe.only('Component <UIScrollAreaView />', () => {
+describe('Component <UIScrollAreaView />', () => {
   it('should render child components', () => {
     const wrapper = enzyme.shallow(<UIScrollAreaView><ChildComponent /></UIScrollAreaView>)
     expect(wrapper.exists(ChildComponent)).to.be.true
   })
   it('should handle className property', () => {
     const className = 'custom'
-    const wrapper = enzyme.shallow(<UIScrollAreaView className={ className } />)
-    expect(wrapper.dive().find('.' + className)).to.have.length(1)
+    const wrapper = enzyme.render(<UIScrollAreaView className={ className }><ChildComponent /></UIScrollAreaView>)
+    expect(wrapper.find('.' + className)).to.have.length(1)
   })
 })
