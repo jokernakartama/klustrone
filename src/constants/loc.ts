ee9c9f6ab066cb19465999c2b592a6bd0474c978
zzap <eat.yummies@gmail.com>
2018-07-07 20:45:51 +0300

    Changed localizations

--- src/constants/loc.ts
+++ src/constants/loc.ts
@@ -1,5 +1,6 @@
 enum loc {
   ACTION_CANCEL = 'Cancel',
+  ACTION_UPDATE = 'Update',
   ACTION_PURGE = 'Purge trash',
   ACTION_INFO = 'Resource info',
   ACTION_MAKEDIR = 'Make a dir',
@@ -12,12 +13,21 @@ enum loc {
   ACTION_REMOVE = 'Remove',
 
   LINK_LABEL_HOME = 'Home',
-  IS_LOADING = 'Loading...',
-  PROPERTY_PUBLIC = 'Public',
+  LABEL_SIZE = 'Size',
+  LABEL_DATE_MODIFIED = 'Modified on',
+  LABEL_NAME = 'Name',
+  LABEL_PUBLIC = 'Public',
+  LABEL_PUBLISHED = 'Published',
+  LABEL_UNPUBLISHED = 'Unpublished',
+  LABEL_OK = 'OK',
+  LABEL_QUIT_TO_MAIN = 'Quit to main',
+  LABEL_CLOSE = 'Close',
 
+  IS_LOADING = 'Loading...',
   DIALOG_NEW_NAME = 'Enter new name',
   DIALOG_NEW_DIR = 'Enter the directory name',
   DIALOG_CONFIRM_REMOVE = 'Remove the resource?',
+  DIALOG_CONFIRM_PURGE = 'Purge the trash?',
   DIALOG_SERVICE_UNMOUNTED = 'The service is not mounted, would you like to mount it?',
   DIALOG_SERVICE_UPDATED = 'The service is mounted now, would you like to display the directory resources?',
   DIALOG_CONFIRM = 'Confirm',
@@ -43,6 +53,7 @@ enum loc {
 
   // mobile labels
   M_ACTION_PANEL_UPDATE_BUTTON = 'UPDT',
+  M_ACTION_PANEL_SERVICE_BUTTON = 'MENU',
   M_ACTION_PANEL_VIEW_BUTTON = 'VIEW',
   M_ACTION_PANEL_SORT_BUTTON = 'SORT',
   M_ACTION_PANEL_ACTIONS_BUTTON = 'ACTN',
