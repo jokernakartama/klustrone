af749c4e698fe5341a841da015f9a63540f34751
zzap <eat.yummies@gmail.com>
2018-07-07 20:55:10 +0300

    Modified UIScrollAreaView component appearance

--- src/components/UI/ScrollArea/View/UIScrollAreaView.tsx
+++ src/components/UI/ScrollArea/View/UIScrollAreaView.tsx
@@ -5,26 +5,29 @@ import bem from '~/utils/bemName'
 
 const UIScrollAreaView: React.SFC<IUIScrollAreaViewComponent.Props> = ({ style, className, children }) => {
   const blockName = 'scroll-area'
-  const scrollAreaClassNames = [
+  const scrollAreaClassNames: IBEMEntityObject[] = [
     { block: blockName }
   ]
   if (className) scrollAreaClassNames.push({ block: className })
-  const trackHClassName = { block: blockName, elem: 'track', mod: { position: 'horizontal' }}
-  const trackVClassName = { block: blockName, elem: 'track', mod: { position: 'vertical' }}
-  const thumbHClassName = { block: blockName, elem: 'thumb', mod: { position: 'horizontal' }}
-  const thumbVClassName = { block: blockName, elem: 'thumb', mod: { position: 'vertical' }}
+  const wrapperClass = bem({ block: blockName + '-wrapper' })
+  const contentClass = bem({ block: blockName, elem: 'content'})
+  const trackHClass = bem({ block: blockName, elem: 'track', mod: { position: 'horizontal' } })
+  const trackVClass = bem({ block: blockName, elem: 'track', mod: { position: 'vertical' } })
+  const thumbHClass = bem({ block: blockName, elem: 'thumb', mod: { position: 'horizontal' } })
+  const thumbVClass = bem({ block: blockName, elem: 'thumb', mod: { position: 'vertical' } })
 
   const scrollArea = (props) => <div {...props} className={ bem(scrollAreaClassNames) } />
-  const scrollAreaTrackHorizontal = (props) => <div {...props} className={ bem(trackHClassName) } />
-  const scrollAreaTrackVertical = (props) => <div {...props} className={ bem(trackVClassName) } />
-  const scrollAreaThumbHorizontal = (props) => <div {...props} className={ bem(thumbHClassName) } />
-  const scrollAreaThumbVertical = (props) => <div {...props} className={ bem(thumbVClassName) } />
+  const scrollAreaTrackHorizontal = (props) => <div {...props} className={ trackHClass } />
+  const scrollAreaTrackVertical = (props) => <div {...props} className={ trackVClass } />
+  const scrollAreaThumbHorizontal = (props) => <div {...props} className={ thumbHClass } />
+  const scrollAreaThumbVertical = (props) => <div {...props} className={ thumbVClass } />
 
   const defaults = {
     autoHide: true,
-    autoHeight: false,
+    className: wrapperClass,
+    autoHeight: true,
     autoHeightMin: 0,
-    autoHeightMax: 500,
+    autoHeightMax: 'unset',
     renderView: scrollArea,
     thumbMinSize: 30,
     renderTrackHorizontal: (props) => scrollAreaTrackHorizontal(props),
@@ -34,8 +37,8 @@ const UIScrollAreaView: React.SFC<IUIScrollAreaViewComponent.Props> = ({ style, 
   }
 
   return (
-    <Scrollbars {...defaults} style={ style }>
-      { children }
+    <Scrollbars { ...defaults } style={ {...style, height: '100%' } }>
+      <div className={ contentClass }>{ children }</div>
     </Scrollbars>
   )
 }
