0643c232250fb805f8b9dbcf2686c4840c8ec872
zzap <eat.yummies@gmail.com>
2018-07-16 23:27:12 +0300

    Changed FileManager views

--- src/components/FileManager/Updated/View/FileManagerUpdatedView.tsx
+++ src/components/FileManager/Updated/View/FileManagerUpdatedView.tsx
@@ -1,5 +1,6 @@
 import './styles/FileManagerUpdatedView.styl'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { loc } from '~/constants'
 import UIIcon from '~/components/UI/Icon'
 import bem from '~/utils/bemName'
@@ -8,14 +9,18 @@ const BLOCK_NAME = 'service-special-message'
 
 const FileManagerUpdatedView: React.SFC<IFileManagerUpdatedViewComponent.Props> = (props) => {
   const iconClass = bem({ block: BLOCK_NAME, elem: 'icon' })
-  const wrapperClass = bem({ block: BLOCK_NAME, elem: 'wrapper' })
+  const messageClass = bem({ block: BLOCK_NAME, elem: 'message' })
+  const btnWrapperClass = bem({ block: BLOCK_NAME, elem: 'buttons' })
   const buttonClass = bem([{ block: BLOCK_NAME, elem: 'button' }, { block: 'button' }])
   return (
     <div className={ BLOCK_NAME }>
-      <div className={ wrapperClass }>
+      <div className={ messageClass }>
         <UIIcon icon='mounted' className={ iconClass } />
         { loc.DIALOG_SERVICE_UPDATED }
-        <button className={ buttonClass } onClick={ props.action }>{ loc.DIALOG_CONFIRM }</button>
+      </div>
+      <div className={ btnWrapperClass }>
+        <Link to='/' className={ buttonClass }>{ loc.M_NAVI_HOME }</Link>
+        <button className={ buttonClass } onClick={ props.action }>{ loc.LABEL_OK }</button>
       </div>
     </div>
   )
